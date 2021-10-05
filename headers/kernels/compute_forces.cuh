31a2d7683d870d9c0109d251997e614400c98533
Олексій Ігнатенко <oleksii.ihnatenko@gmail.com>
2021-10-05 23:48:24 +0300

    Minor changes

--- headers/kernels/compute_forces.cuh
+++ headers/kernels/compute_forces.cuh
@@ -14,26 +14,25 @@ template<typename particle, typename force>
 __global__ void compute_interparticle_forces(const particle* particles, force* forces, unsigned* locks)
 {
 	unsigned local_id = threadIdx.x;
-	unsigned global_id = local_id + BLK_SIZE * blockIdx.y;
-	unsigned global_cur = local_id + BLK_SIZE * blockIdx.x;
+	unsigned global_id = local_id + BLK_SIZE * blockIdx.x;
+	unsigned global_load = local_id + BLK_SIZE * blockIdx.y;
 
-	force force{};
-	particle par = particles[global_cur];
+	force frc{};
+	particle ptc = particles[global_id];
 
-	__shared__ particle par_cache[BLK_SIZE];
-
-	par_cache[local_id] = particles[global_id];
+	__shared__ particle ptc_cache[BLK_SIZE];
+	ptc_cache[local_id] = particles[global_load];
 	__syncthreads();
 
-	for (unsigned index = local_id + BLK_SIZE - (global_cur == global_id);
-		index != local_id; --index)
-		force += par_cache[index % BLK_SIZE].force_on(par);
+	for (unsigned local_cur = local_id + BLK_SIZE - (blockIdx.x == blockIdx.y);
+		local_cur != local_id; --local_cur)
+		frc += ptc_cache[local_cur % BLK_SIZE].force_on(ptc);
 
 	if (local_id == 0)
 		while (atomicCAS(locks + blockIdx.x, gridDim.x, blockIdx.x) != blockIdx.x);
 	__syncthreads();
 
-	forces[global_cur] += force;
+	forces[global_id] += frc;
 	__threadfence();
 
 	if (local_id == 0)
